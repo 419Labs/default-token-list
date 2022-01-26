50fe4ef6ff5541f2dd4a9499e5e880c428d6618a
Lenny Aebischer <lenny.aebischer@studio419.ch>
2022-01-26 20:31:27 +0100

    Manage L1/L2 output | Adapt test for both layers

--- src/buildList.js
+++ src/buildList.js
@@ -1,16 +1,17 @@
 const { version } = require("../package.json");
-const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
-const polygon = require("./tokens/polygon.json");
-const mumbai = require("./tokens/mumbai.json");
+const mainnetL1 = require("./tokens/L1/mainnet.json");
+const ropstenL1 = require("./tokens/L1/ropsten.json");
+const rinkebyL1 = require("./tokens/L1/rinkeby.json");
+const goerliL1 = require("./tokens/L1/goerli.json");
+const goerliL2 = require("./tokens/L2/starknet/goerli.json");
+const kovanL1 = require("./tokens/L1/kovan.json");
+const polygonL1 = require("./tokens/L1/polygon.json");
+const mumbaiL1 = require("./tokens/L1/mumbai.json");
 
-module.exports = function buildList() {
+function buildList(layer, tokens) {
   const parsed = version.split(".");
   return {
-    name: "419Labs List",
+    name: "419Labs List for " + layer,
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -19,14 +20,28 @@ module.exports = function buildList() {
     },
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
-    keywords: ["419Labs", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai]
-      // sort them by symbol for easy readability
-      .sort((t1, t2) => {
-        if (t1.chainId === t2.chainId) {
-          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
-        }
-        return t1.chainId < t2.chainId ? -1 : 1;
-      }),
+    keywords: ["419Labs", "default", layer],
+    tokens: tokens
+        // sort them by symbol for easy readability
+        .sort((t1, t2) => {
+          if (t1.chainId === t2.chainId) {
+            return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+          }
+          return t1.chainId < t2.chainId ? -1 : 1;
+        }),
   };
-};
+}
+
+function buildListL1() {
+    return buildList("L1", [...mainnetL1, ...ropstenL1, ...goerliL1, ...kovanL1, ...rinkebyL1, ...polygonL1, ...mumbaiL1]);
+}
+
+function buildListL2() {
+  return buildList("L2", [...goerliL2]);
+}
+
+
+module.exports = {
+  buildListL1,
+  buildListL2
+}
--- src/write.js
+++ src/write.js
@@ -1,2 +1,12 @@
-const buildList = require('./buildList');
-console.log(JSON.stringify(buildList(), null, 2));
+const {buildListL1, buildListL2} = require('./buildList');
+const layer = process.argv[2];
+switch (layer) {
+    case "L1":
+        console.log(JSON.stringify(buildListL1(), null, 2));
+        break;
+    case "L2":
+        console.log(JSON.stringify(buildListL2(), null, 2));
+        break;
+    default:
+        console.log("Please specify layer to output")
+}
